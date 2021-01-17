ac0ea3d3bf5574da79bffd83ce158ea15159418f
Marcos Villar <marcosvsantiso@gmail.com>
2021-01-17 21:16:34 +0100

    New version of my database

--- BD/HACKATONES_V1.3.sql
+++ BD/HACKATONES_V1.3.sql
@@ -24,17 +24,17 @@ DROP TABLE IF EXISTS `hackaton`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hackaton` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   `presencial` tinyint(1) NOT NULL,
-  `f_inicio` date NOT NULL,
-  `f_fin` date NOT NULL,
-  `h_inicio` time NOT NULL,
-  `h_fin` time NOT NULL,
-  `ciudad` varchar(20) DEFAULT NULL,
+  `ciudad` varchar(255) DEFAULT NULL,
   `contenido` varchar(2000) NOT NULL,
   `id_tema` mediumint DEFAULT NULL,
   `id_user` mediumint DEFAULT NULL,
   `id_tech` mediumint DEFAULT NULL,
+  `inicio` timestamp NOT NULL,
+  `fin` timestamp NOT NULL,
+  `avatar` blob,
+  `max_register` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_tema` (`id_tema`),
   KEY `id_user` (`id_user`),
@@ -42,7 +42,7 @@ CREATE TABLE `hackaton` (
   CONSTRAINT `hackaton_ibfk_1` FOREIGN KEY (`id_tema`) REFERENCES `tematica` (`id`),
   CONSTRAINT `hackaton_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id`),
   CONSTRAINT `hackaton_ibfk_3` FOREIGN KEY (`id_tech`) REFERENCES `tecnologia` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +63,22 @@ CREATE TABLE `hackaton_tiene_tecnologia` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `noticias`
+--
+
+DROP TABLE IF EXISTS `noticias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noticias` (
+  `id` mediumint NOT NULL AUTO_INCREMENT,
+  `titular` varchar(500) NOT NULL,
+  `contenido` varchar(5000) NOT NULL,
+  `f_publicacion` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `tecnologia`
 --
 
@@ -71,7 +87,7 @@ DROP TABLE IF EXISTS `tecnologia`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tecnologia` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(200) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -85,7 +101,7 @@ DROP TABLE IF EXISTS `tematica`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tematica` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -99,21 +115,23 @@ DROP TABLE IF EXISTS `usuario`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido1` varchar(20) DEFAULT NULL,
-  `apellido2` varchar(20) DEFAULT NULL,
-  `dni` varchar(30) DEFAULT NULL,
-  `calle` varchar(100) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido1` varchar(255) DEFAULT NULL,
+  `apellido2` varchar(255) DEFAULT NULL,
+  `dni` varchar(255) DEFAULT NULL,
+  `calle` varchar(255) DEFAULT NULL,
   `numero` smallint DEFAULT NULL,
-  `ciudad` varchar(20) DEFAULT NULL,
-  `nick` varchar(30) NOT NULL,
-  `pass` varchar(20) NOT NULL,
+  `ciudad` varchar(255) DEFAULT NULL,
+  `nick` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `bio` varchar(500) DEFAULT NULL,
   `score` int DEFAULT NULL,
+  `avatar` blob,
+  `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nick` (`nick`),
   CONSTRAINT `usuario_chk_1` CHECK ((`numero` > 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +144,7 @@ DROP TABLE IF EXISTS `usuario_apuntado_a_hackaton`;
 CREATE TABLE `usuario_apuntado_a_hackaton` (
   `id_user` mediumint NOT NULL,
   `id_hack` mediumint NOT NULL,
-  `c_reserva` varchar(50) DEFAULT NULL,
+  `c_reserva` varchar(255) DEFAULT NULL,
   `posicion` mediumint DEFAULT NULL,
   `score_hack` int DEFAULT NULL,
   PRIMARY KEY (`id_user`,`id_hack`),
@@ -145,4 +163,4 @@ CREATE TABLE `usuario_apuntado_a_hackaton` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-27 23:10:51
+-- Dump completed on 2021-01-17 21:02:46
